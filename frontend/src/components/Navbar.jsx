f81a8c97dc22472c7bb29e54607c99f885b9b121
amir-khusroo <amirkhusroo250@gmail.com>
2025-01-05 00:38:10 +0530

    error changes

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import { Link, NavLink, useNavigate} from 'react-router-dom'
 import LoginLogoutButton from './LoginLogoutButton';
 import axios from 'axios';
 import { toast } from 'react-toastify';
-import logo from './assets/logo.jpg';
+import logo from '../assets/logo.jpg';
 const API_URL = import.meta.env.VITE_API_URL;
 
 const Navbar=()=> {
